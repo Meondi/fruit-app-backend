8aee9eb8df63ec3e9d88917e700cec37a1b2858b
Mike Bastian <mike.bastian@meondi.com>
2023-03-31 16:46:05 +0200

    Remove unneeded code

--- Package.swift
+++ Package.swift
@@ -28,9 +28,5 @@ let package = Package(
             ]
         ),
         .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
     ]
 )
